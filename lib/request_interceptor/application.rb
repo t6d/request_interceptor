7fa48335862124f22993eb32dead9793b98afb00
Konstantin Tennhard <me@t6d.de>
2015-10-15 16:18:19 -0400

    Application - Raise errors by default

--- lib/request_interceptor/application.rb
+++ lib/request_interceptor/application.rb
@@ -4,4 +4,9 @@ class RequestInterceptor::Application < Sinatra::Base
   class << self
     attr_accessor :hostname_pattern
   end
+
+  configure do
+    disable :show_exceptions
+    enable :raise_errors
+  end
 end
