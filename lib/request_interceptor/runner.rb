f2d69ee7326601043c35833ebbb256e755e71889
Kevin Hughes <kevin.hughes@shopify.com>
2015-10-15 18:52:00 +0000

    refactor run into methods

--- lib/request_interceptor/runner.rb
+++ lib/request_interceptor/runner.rb
@@ -16,34 +16,11 @@ class RequestInterceptor::Runner
   end
 
   def run(&simulation)
-    runner = self
-
-    original_request_method = Net::HTTP.instance_method(:request)
-    original_start_method = Net::HTTP.instance_method(:start)
-    original_finish_method = Net::HTTP.instance_method(:finish)
-
-    Net::HTTP.class_eval do
-      def start
-        @started = true
-        return yield(self) if block_given?
-        self
-      end
-
-      def finish
-        @started = false
-        nil
-      end
-
-      define_method(:request) do |request, body = nil, &block|
-        runner.request(request, body, &block)
-      end
-    end
-
+    cache_original_nethttp_methods
+    override_nethttp_methods
     simulation.call
   ensure
-    Net::HTTP.send(:define_method, :request, original_request_method)
-    Net::HTTP.send(:define_method, :start, original_start_method)
-    Net::HTTP.send(:define_method, :finish, original_finish_method)
+    restore_nethttp_methods
   end
 
   def request(request, body, &block)
@@ -73,5 +50,40 @@ class RequestInterceptor::Runner
     block.call(response) if block
 
     response
+  end
+
+  private
+
+  def cache_original_nethttp_methods
+    @original_request_method = Net::HTTP.instance_method(:request)
+    @original_start_method = Net::HTTP.instance_method(:start)
+    @original_finish_method = Net::HTTP.instance_method(:finish)
+  end
+
+  def override_nethttp_methods
+    runner = self
+
+    Net::HTTP.class_eval do
+      def start
+        @started = true
+        return yield(self) if block_given?
+        self
+      end
+
+      def finish
+        @started = false
+        nil
+      end
+
+      define_method(:request) do |request, body = nil, &block|
+        runner.request(request, body, &block)
+      end
+    end
+  end
+
+  def restore_nethttp_methods
+    Net::HTTP.send(:define_method, :request, @original_request_method)
+    Net::HTTP.send(:define_method, :start, @original_start_method)
+    Net::HTTP.send(:define_method, :finish, @original_finish_method)
   end
 end
