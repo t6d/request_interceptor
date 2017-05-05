cf0926c3dd7cd1d79550ef79c37a6af3cc8177a6
Konstantin Tennhard <konstantin@tennhard.net>
2017-05-05 15:44:11 -0400

    Manage WebMock configuration alterations differently

--- lib/request_interceptor/setup_webmock.rb
+++ lib/request_interceptor/setup_webmock.rb
@@ -1,7 +1,5 @@
 class RequestInterceptor::SetupWebmock
-  WebMockSettings = Struct.new(:request_stubs, :callbacks, :net_connect_allowed, :show_body_diff, :show_stubbing_instructions) do
-    alias net_connect_allowed? net_connect_allowed
-  end
+  WebMockSettings = Struct.new(:request_stubs, :callbacks, :allow_net_connect, :allow_localhost, :show_body_diff, :show_stubbing_instructions)
 
   def self.perform(applications, callback = nil, &simulation)
     new(applications, callback).perform(&simulation)
@@ -32,7 +30,8 @@ class RequestInterceptor::SetupWebmock
   def setup_webmock
     webmock_settings.request_stubs = WebMock::StubRegistry.instance.request_stubs.dup || []
     webmock_settings.callbacks = WebMock::CallbackRegistry.callbacks.dup || []
-    webmock_settings.net_connect_allowed = WebMock.net_connect_allowed?
+    webmock_settings.allow_net_connect = WebMock::Config.instance.allow_net_connect
+    webmock_settings.allow_localhost = WebMock::Config.instance.allow_localhost
     webmock_settings.show_body_diff = WebMock::Config.instance.show_body_diff
     webmock_settings.show_stubbing_instructions = WebMock::Config.instance.show_stubbing_instructions
 
@@ -49,7 +48,8 @@ class RequestInterceptor::SetupWebmock
   end
 
   def reset_webmock
-    WebMock.disable_net_connect! unless webmock_settings.net_connect_allowed?
+    WebMock::Config.instance.allow_net_connect = webmock_settings.allow_net_connect
+    WebMock::Config.instance.allow_localhost = webmock_settings.allow_localhost
     WebMock::Config.instance.show_body_diff = webmock_settings.show_body_diff
     WebMock::Config.instance.show_stubbing_instructions = webmock_settings.show_stubbing_instructions
     WebMock::CallbackRegistry.reset
@@ -58,5 +58,4 @@ class RequestInterceptor::SetupWebmock
     end
     WebMock::StubRegistry.instance.request_stubs = webmock_settings.request_stubs
   end
-
 end
